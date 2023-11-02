bb77a2af2f9a94d84ad1b5882c7427ef8352bc9f
Jesus PM - UNIR <jesus.perezmelero@unir.net>
2023-11-02 22:18:31 +0100

    Init repo

---
+++ src/main/java/com/unir/app/MySqlApplication.java
@@ -0,0 +1,42 @@
+package com.unir.app;
+
+import com.unir.config.HibernateMySqlConfig;
+import com.unir.config.LogbackConfig;
+import com.unir.dao.DepartmentsDao;
+import com.unir.dao.EmployeesDao;
+import com.unir.model.mysql.Department;
+import com.unir.model.mysql.Employee;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import java.util.List;
+
+@Slf4j
+public class MySqlApplication {
+
+    public static void main(String[] args) {
+
+        LogbackConfig.configureLogbackForHibernateSQL();
+
+        //Try-with-resources. Se cierra la conexión automáticamente al salir del bloque try
+        try (Session session = HibernateMySqlConfig.getSessionFactory().openSession()) {
+
+            log.info("Conexión establecida con la base de datos MySQL");
+
+            //Creamos los DAOs que nos permitirán interactuar con la base de datos
+            EmployeesDao employeesDao = new EmployeesDao(session);
+            DepartmentsDao departmentsDao = new DepartmentsDao(session);
+
+            List<Employee> employees = employeesDao.findByDepartment("d001");
+            log.info("Empleados del departamento d001: {}", employees.size());
+
+            Department bbddDepartment = new Department();
+            bbddDepartment.setDeptName("Database Department");
+            bbddDepartment.setDeptNo("d010");
+            //departmentsDao.save(bbddDepartment);
+            //log.info("Departamento insertado: {}", bbddDepartment);
+
+        } catch (Exception e) {
+            log.error("Error al tratar con la base de datos", e);
+        }
+    }
+}
